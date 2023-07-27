9fc3f8e42c7628769633f3f23540bd2b5ba2cc40
Zagrthos <36556009+Zagrthos@users.noreply.github.com>
2023-07-27 21:25:41 +0200

    Add Chat_Layer.png

--- ChatManager/Properties/Resources.Designer.cs
+++ ChatManager/Properties/Resources.Designer.cs
@@ -79,6 +79,16 @@ namespace ChatManager.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Chat_Layer {
+            get {
+                object obj = ResourceManager.GetObject("Chat_Layer", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] Font {
