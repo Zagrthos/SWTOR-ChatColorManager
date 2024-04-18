e628aa4200621f87035ea04987414459408a4c1f
Zagrthos <36556009+Zagrthos@users.noreply.github.com>
2024-04-18 15:59:39 +0200

    Set Forms to be DPI aware

--- ChatManager/Forms/FileSelectorForm.Designer.cs
+++ ChatManager/Forms/FileSelectorForm.Designer.cs
@@ -399,8 +399,8 @@ namespace ChatManager.Forms
             // 
             // FileSelectorForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             ClientSize = new Size(484, 261);
             Controls.Add(tabsFileSelector);
             FormBorderStyle = FormBorderStyle.FixedDialog;
--- ChatManager/MainForm.Designer.cs
+++ ChatManager/MainForm.Designer.cs
@@ -956,8 +956,8 @@ namespace ChatManager
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
             ClientSize = new Size(484, 328);
             Controls.Add(tlpMainForm);
             FormBorderStyle = FormBorderStyle.FixedSingle;
